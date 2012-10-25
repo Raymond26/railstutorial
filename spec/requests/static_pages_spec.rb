2aad01a091bfd7f99a33c9c0ba200842790547ec
Raymond Lau <raymond.lau.ca@gmail.com>
2012-10-25 16:57:57 -0700

    converted to postgresql

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+	baseTitle = "Ruby on Rails Tutorial Sample App | "
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -10,7 +12,7 @@ describe "StaticPages" do
     it "should have the title 'Home'" do
     	visit '/static_pages/home'
     	page.should have_selector('title',
-    		:text => "Ruby on Rails Tutorial Sample App | Home")
+    		:text => baseTitle + "Home")
     end
   end
 
@@ -22,7 +24,7 @@ describe "StaticPages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Sample App | Help")
+        :text => baseTitle + "Help")
     end
   end
 
@@ -34,8 +36,20 @@ describe "StaticPages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Sample App | About Us")
+        :text => baseTitle + "About Us")
     end
+  end
+
+  describe "Contact page" do
+  	it "should have the content 'Contact List'" do
+  		visit '/static_pages/contact'
+  		page.should have_content('Contact List')
+  	end
+  	it "should have the title 'Contact'" do
+  		visit '/static_pages/contact'
+  		page.should have_selector('title',
+  			:text => baseTitle + "Contact")
+  	end
   end
 
 end
